092aee35b91636ce837157d39a4fc62886695182
CodingSeo <seojingyo@hotmail.com>
2021-01-12 21:12:09 +0900

    init project

---
+++ app/Http/Controllers/Documentation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Documentation extends Controller
+{
+
+    public function document(Request $request)
+    {
+        var_dump('test');
+    }
+}
